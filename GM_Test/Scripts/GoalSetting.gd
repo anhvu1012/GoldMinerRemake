cb67c87b8de26caf6d20c3f72f03c1c78fbdc7af
Anh <92798974+anhvu1012@users.noreply.github.com>
2023-06-14 20:57:42 +0200

    Delete GM_Test directory

--- GM_Test/Scripts/GoalSetting.gd
+++
@@ -1,4 +0,0 @@
-extends Label
-
-func _process(_delta):
-	self.text = "Goal: " + "5000" #for now, later it will be GameManager.goal
--- GM_Test/Scripts/LevelSetting.gd
+++
@@ -1,4 +0,0 @@
-extends Label
-
-func _process(_delta):
-	self.text = "Level: " + "1" #for now, later it will be GameManager.current_level
